77ad9e23dc9115ea4730463cbf21291b43bc2589
Konstantin Berg <163849606+it2konst@users.noreply.github.com>
2025-03-09 13:58:28 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="scss">
+@use "./assets/styles/index.scss";
+
+body {
+  background-color: #aaa;
+}
+</style>

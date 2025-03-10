749fda81deb28079ff3aaf9185d33868668ab2ec
Konstantin Berg <163849606+it2konst@users.noreply.github.com>
2025-03-10 18:47:54 +0700

    feat: AppMenu

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div class="menu">
+    <div class="logo">Logo</div>
+
+    <el-menu class="nav el-menu-vertical-demo">
+      <el-menu-item index="1">
+        <el-icon :size="24"><Food /></el-icon>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <el-icon :size="24"><Dish /></el-icon>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/index" as *;
+
+.menu {
+  height: 100vh;
+  background-color: $violet;
+}
+</style>
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,20 @@
-<script setup></script>
+<script setup>
+import AppMenu from "@/components/AppMenu.vue";
+</script>
 
 <template>
-  <p>111</p>
+  <div class="root">
+    <div class="menu-container">
+      <AppMenu />
+    </div>
+    <main></main>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use "@/assets/styles/index";
+
+.menu-container {
+  width: 70px;
+}
 </style>

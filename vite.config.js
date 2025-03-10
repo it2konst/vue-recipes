be93c7dedb027206cafd84db7265d00240c814c4
Konstantin Berg <163849606+it2konst@users.noreply.github.com>
2025-03-10 22:40:36 +0700

    feat: AppMenu

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const extType = assetInfo.name.split(".").pop();
+          if (extType === "css") {
+            return "assets/styles/[name].[hash][extname]";
+          }
+          if (["png", "jpg", "jpeg", "gif", "webp", "svg"].includes(extType)) {
+            return "assets/images/[name].[hash][extname]";
+          }
+          if (["woff", "woff2", "eot", "ttf", "otf"].includes(extType)) {
+            return "assets/fonts/[name].[hash][extname]";
+          }
+          return "assets/[name].[hash][extname]";
+        },
+        chunkFileNames: "assets/js/[name].[hash].js",
+        entryFileNames: "assets/js/[name].[hash].js",
+      },
+    },
+  },
+});

c1bf6e14fca3a316cf79044e7f8f52d160abb14e
Konstantin Berg <163849606+it2konst@users.noreply.github.com>
2025-03-16 08:59:52 +0700

    feat: Recipe table

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -27,11 +27,52 @@ onMounted(() => {
       <app-button text="Добавить рецепт"></app-button>
     </template>
     <template #inner>
-      {{ recipes }}
+      <div class="wrapper">
+        <el-table :data="recipes" style="width: 100%">
+          <el-table-column prop="idMeal" label="Id" />
+          <el-table-column label="Image">
+            <template #default="scope">
+              <img :src="scope?.row?.strMealThumb" alt="img" class="image" />
+            </template>
+          </el-table-column>
+
+          <el-table-column prop="strMeal" label="Name" />
+          <el-table-column prop="strArea" label="Aria" />
+          <el-table-column prop="strCategory" label="Category" />
+          <el-table-column label="Tags">
+            <template #default="scope">
+              <template v-if="scope?.row?.strTags">
+                <el-tag
+                  v-for="(tag, key) in scope.row.strTags.split(',')"
+                  :key="key"
+                  type="primary"
+                  class="tag"
+                  >{{ tag }}</el-tag
+                >
+              </template>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </template>
   </app-layout>
 </template>
 
 <style lang="scss" scoped>
 @use "@/assets/styles/index" as *;
+
+.image {
+  width: 70px;
+  height: auto;
+
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: center;
+
+  border: 1px solid $violet;
+}
+
+.tag {
+  margin-right: 2px;
+}
 </style>
--- src/services/RecipeService/provider.js
+++ src/services/RecipeService/provider.js
@@ -3,7 +3,5 @@ import { URL_RECIPES_BY_LETTER } from "@/constants";
 
 export const getRecipesByLetter = async (letter) => {
   const response = await axios.get(URL_RECIPES_BY_LETTER);
-  return response.data;
+  return response.data?.meals;
 };
-
-// export { getRecipesByLetter };
